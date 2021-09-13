0ec9562ed7abbe065406b559c1df7599c51ea5b1
aniakacp <aniakacp@op.pl>
2021-09-14 00:25:38 +0200

    upload

---
+++ scraping_site/scraping_site_program/README.md
@@ -0,0 +1,19 @@
+# Scraping Site
+
+- Ideas on how the software can be improved in the future.
+- Are there any risks associated with using such a program?
+
+## Table of Contents
+* [General info](#general-info)
+* [Scope of functionalities](#scope-of-functionalities)
+* [Illustrations](#illustrations)
+* [Project status](#project-status)
+
+### General info
+Program scraping the google site looking for specific words from the text document.
+Results are saved in a csv file with a specific name of the file.
+Code has comments on functions/classes.
+
+
+### Project status
+In progress
--- scraping_site/scraping_site_program/scraping_text.py
+++ scraping_site/scraping_site_program/scraping_text.py
@@ -7,6 +7,9 @@ import os
 import requests
 
 class ScrapingText:
+    """
+    Class with keywords
+    """
     def __init__(self):
        self.keywords=[]
 
@@ -17,23 +20,9 @@ class ScrapingText:
         """
         return [r.strip() for r in row]
 
-    def get_data(self, file_name="/home/aniakacp/scraping_site/scraping_site_program/files/keywords.text"):
-
-        with open(file_name, newline="") as _file:
-            reader = csv.reader(
-                _file,
-                delimiter=",",
-                quotechar='"',
-                quoting=csv.QUOTE_ALL,
-                skipinitialspace=True,
-            )
-            lines = _file.readlines()
-            self.headers= lines[0].split(', ' '')
-
-
     def read_file(self, file_name="/home/aniakacp/scraping_site/scraping_site_program/files/keywords.txt"):
         """
-            Fetch the data from the given txt file and construct the list of Student objects
+            Fetch the data from the given txt file with keywords and get the list of keywords
         """
         with open(file_name, "r") as f:
             lines = f.readlines()
@@ -43,20 +32,30 @@ class ScrapingText:
         f.close()
         return self.keywords
 
-    def search_site(self, keywords):
-        response= requests.get('http://www.google.com/search?start=0&num=10&q=red+sox&cr=countryCA')
-        return response.request
+   # def search_site(self, keywords):
+   #     response= requests.get('http://www.google.com/search?start=0&num=10&q=red+sox&cr=countryCA')
+   #     return response.request
 
     def create_file(self, filename):
+        """
+        :param filename: filename
+        Create file if it doesn't exist.
+        """
         if not os.path.exists(filename):
             open(filename, 'w').close()
 
-    def googleSearch(self, keywords=None, file_name="/home/aniakacp/scraping_site/scraping_site_program/files/results.csv"):
-        link = 'https://www.searchenginejournal.com/'
+    def googleSearch(self):
+        """
+            Create csv files with the name of the keyword.
+            Search in the google site for 'link + keyword".
+            Print results containing the name of the link in new files
+        """
+      #  link = 'https://www.searchenginejournal.com/'
         for keyword in self.keywords:
-            filename= f"/home/aniakacp/scraping_site/scraping_site_program/files/{keyword}.txt"
+            filename= f"/home/aniakacp/scraping_site/scraping_site_program/files/{keyword}.csv"
             self.create_file(filename= filename)
-            for j in search(query = '?q=https://www.searchenginejournal.com/&q=screaming+frog', tld="co.in", num=10, stop=10, pause=2, extra_params= {'filter': link}):
+            keyword = keyword.replace(" ", "+")
+            for j in search(query = f'?q=https://www.searchenginejournal.com/&q={keyword}', tld="co.in", num=10, stop=10, pause=2): #, extra_params= {'filter': link}):
                 with open(filename, "a") as f:
                     if j.startswith('https://www.searchenginejournal.com/'):
                         f.writelines(f'{j}/\n')
@@ -65,21 +64,17 @@ class ScrapingText:
                 f.close()
 
 
-    def check(self):
-        URL = "https://www.google.com/search?q=https://www.searchenginejournal.com/&q=screaming+frog"
-        r = requests.get(URL)
-        print(r.content)
+   # def soup(self):
+   #     r = requests.get('https://www.google.com/search?q=https://www.searchenginejournal.com/&q=screaming+frog')
+   #     soup = BeautifulSoup(r.content, 'html5lib')
+   #     print(soup.prettify())
+   #     if soup.find("div", {"id": "result-stats"}):
+   #         print(soup.find("div", {"id": "result-stats"}))
 
-    def soup(self):
-        r = requests.get('https://www.google.com/search?q=https://www.searchenginejournal.com/&q=screaming+frog')
-        soup = BeautifulSoup(r.content, 'html5lib')
-        print(soup.prettify())
-        if soup.find("div", {"id": "result-stats"}):
-            print(soup.find("div", {"id": "result-stats"}))
 
+# To execute the code:
 if __name__ == "__main__":
     a = ScrapingText()
     a.read_file()
-  #  a.googleSearch()
-    a.soup()
+    a.googleSearch()
 
